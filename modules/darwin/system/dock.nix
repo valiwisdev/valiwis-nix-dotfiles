891e10124d7282dcde66c2e9ff665f7cfb6dd922
valiwisdev <v.caro@uniandes.edu.co>
2026-04-22 02:39:45 -0500

    Update dock configuration to include Spotify and Discord as persistent apps

--- modules/darwin/system/dock.nix
+++ modules/darwin/system/dock.nix
@@ -4,9 +4,13 @@
     autohide = true;
     show-recents = false;
     showhidden = true; 
-    
+
     persistent-apps = [
       "/Applications/Microsoft Edge.app"
+      "/Applications/Spotify.app"
+      "/Applications/Discord.app"
+      "/Applications/ChatGPT.app"
+      "/Applications/Notion.app"
       "/Applications/Visual Studio Code.app"
     ];
 
