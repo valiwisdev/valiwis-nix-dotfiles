2afcd811a44c909c6ec916298a4538a5849056b4
valiwisdev <v.caro@uniandes.edu.co>
2026-04-22 01:14:02 -0500

    Fix syntax error in vscode.nix by removing redundant 'with' statement

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -1,8 +1,10 @@
+{ pkgs, ... }:
+
 {
     programs.vscode = {
         enable = true;
         profiles.default = {
-            extensions = with with pkgs.vscode-marketplace; [
+            extensions = with pkgs.vscode-marketplace; [
                 catppuccin.catppuccin-vsc
             ];
         };
