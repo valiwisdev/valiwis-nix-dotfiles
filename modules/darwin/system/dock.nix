bda7c0398d2d24febafd29945beff685f4f19ac9
valiwisdev <v.caro@uniandes.edu.co>
2026-05-04 15:55:34 -0500

    Add Microsoft Word, Excel, OBS, and Docker to persistent apps in dock configuration

--- modules/darwin/system/dock.nix
+++ modules/darwin/system/dock.nix
@@ -9,11 +9,15 @@
     persistent-apps = [
       "/System/Applications/App Store.app"
       "/Applications/Microsoft Edge.app"
+      "/Applications/Microsoft Word.app"
+      "/Applications/Microsoft Excel.app"
       "/Applications/Spotify.app"
       "/Applications/Discord.app"
       "/Applications/ChatGPT.app"
       "/Applications/Notion.app"
+      "/Applications/OBS.app"
       "${home}/Applications/Home Manager Apps/Visual Studio Code.app"
+      "/Applications/Docker.app"
       "/Applications/Ghostty.app"
     ];
 
