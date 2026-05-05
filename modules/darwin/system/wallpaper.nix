fb72d78572de429bd508d1fbf1eda0392c0501ad
valiwisdev <v.caro@uniandes.edu.co>
2026-05-05 18:19:42 -0500

    Refactor wallpaper configuration to support dynamic wallpaper selection

--- modules/darwin/system/wallpaper.nix
+++ modules/darwin/system/wallpaper.nix
@@ -1,26 +1,55 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
-  wallpaper = ./wallpapers/catppuccintotoro.png;
+  cfg = config.my.wallpaper;
+
+  wallpapersDir = ./wallpapers;
+
+  wallpaperFiles =
+    builtins.attrNames
+      (lib.filterAttrs
+        (name: type:
+          type == "regular"
+          && (
+            lib.hasSuffix ".png" name
+            || lib.hasSuffix ".jpg" name
+            || lib.hasSuffix ".jpeg" name
+            || lib.hasSuffix ".heic" name
+            || lib.hasSuffix ".webp" name
+          )
+        )
+        (builtins.readDir wallpapersDir));
+
+  selectedWallpaper = "${wallpapersDir}/${cfg.name}";
 
   setWallpaper = pkgs.writeShellScriptBin "set-wallpaper" ''
     set -euo pipefail
 
-    /usr/bin/osascript -e 'tell application "Finder" to set desktop picture to POSIX file "${wallpaper}"'
+    /usr/bin/osascript -e 'tell application "Finder" to set desktop picture to POSIX file "${selectedWallpaper}"'
   '';
 in
 {
-  environment.systemPackages = [
-    setWallpaper
-  ];
+  options.my.wallpaper = {
+    name = lib.mkOption {
+      type = lib.types.enum wallpaperFiles;
+      default = "catppuccintotoro.png";
+      description = "Wallpaper file to use from the wallpapers folder.";
+    };
+  };
 
-  system.activationScripts.setWallpaper.text = ''
-    echo "Setting wallpaper..." >&2
+  config = {
+    environment.systemPackages = [
+      setWallpaper
+    ];
 
-    user="${config.system.primaryUser}"
-    uid="$(id -u "$user")"
+    system.activationScripts.setWallpaper.text = ''
+      echo "Setting wallpaper to ${cfg.name}..." >&2
 
-    launchctl asuser "$uid" sudo -u "$user" \
-      ${setWallpaper}/bin/set-wallpaper || true
-  '';
+      user="${config.system.primaryUser}"
+      uid="$(id -u "$user")"
+
+      launchctl asuser "$uid" sudo -u "$user" \
+        ${setWallpaper}/bin/set-wallpaper || true
+    '';
+  };
 }
