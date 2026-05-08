5ec8434c5b104984e26602d4378b284cfb5697f9
valiwisdev <v.caro@uniandes.edu.co>
2026-05-08 17:00:56 -0500

    Add masApps import and define Microsoft Office applications in masApps.nix

--- modules/darwin/homebrew/default.nix
+++ modules/darwin/homebrew/default.nix
@@ -4,6 +4,7 @@
 
     brews = import ./brews.nix;
     casks = import ./casks.nix;
+    masApps = import ./mas-apps.nix;
 
     onActivation.cleanup = "zap";
     onActivation.autoUpdate = true;
---
+++ modules/darwin/homebrew/masApps.nix
@@ -0,0 +1,4 @@
+{
+  "Microsoft Word " = 462054704;
+  "Microsoft Excel " = 462058435;
+}
