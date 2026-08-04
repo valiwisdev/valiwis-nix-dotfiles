3cc13c01fc3ff638c2de4dc540b2b0e601c80ea7
valiwisdev <v.caro@uniandes.edu.co>
2026-08-03 20:37:40 -0500

    fix(lazy): add lockfile path for lazy.nvim setup

--- modules/home/programs/config/nvim/lua/config/lazy.lua
+++ modules/home/programs/config/nvim/lua/config/lazy.lua
@@ -15,6 +15,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  lockfile = vim.fn.stdpath("state") .. "/lazy/lazy-lock.json",
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
