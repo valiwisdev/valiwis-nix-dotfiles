3ee9cce91524c6a1763fc72a87697389794c3406
valiwisdev <v.caro@uniandes.edu.co>
2026-08-31 15:04:34 -0500

    fix(vscode): remove unnecessary user settings for cleaner configuration

--- modules/home/programs/vscode.nix
+++ modules/home/programs/vscode.nix
@@ -9,30 +9,6 @@
       ];
 
       userSettings = {
-        "chat.viewSessions.orientation": "stacked",
-        "docker.extension.enableComposeLanguageServer": false,
-        "editor.accessibilitySupport": "on",
-        "editor.fontFamily": "'JetBrains Mono', monospace",
-        "editor.fontLigatures": true,
-        "editor.fontSize": 18,
-        "editor.lineHeight": 1.5,
-        "files.autoSave": "afterDelay",
-        "git.autofetch": true,
-        "python.analysis.typeCheckingMode": "basic",
-        "redhat.telemetry.enabled": false,
-        "sonarlint.focusOnNewCode": false,
-        "svelte.enable-ts-plugin": true,
-        "terminal.integrated.env.osx": {
-          "Q_NEW_SESSION": "1"
-        },
-        "terminal.integrated.fontFamily": "'JetBrainsMonoNL NFP', monospace",
-        "terminal.integrated.fontSize": 16,
-        "terminal.integrated.initialHint": false,
-        "update.mode": "none",
-        "workbench.activityBar.location": "top",
-        "workbench.colorTheme": "Catppuccin Mocha",
-        "workbench.sideBar.location": "right"
-        }        
       ;
     };
   };
