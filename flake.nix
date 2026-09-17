6c1263a2d5d73f431f091841f7c276d481da34e9
valiwisdev <v.caro@uniandes.edu.co>
2026-09-17 11:29:01 -0500

    fix(flake): update mac-app-util to follow nixpkgs input

--- flake.nix
+++ flake.nix
@@ -22,6 +22,7 @@
     };
   
     mac-app-util.url = "github:hraban/mac-app-util";
+    mac-app-util.inputs.nixpkgs.follows = "nixpkgs";
     catppuccin.url = "github:catppuccin/nix/release-26.05";
   };
 
